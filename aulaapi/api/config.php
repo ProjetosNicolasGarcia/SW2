eae0e0a44678e4761e3baabf4418e76c997e602b
Nicolas Garcia <100208523+ProjetosNicolasGarcia@users.noreply.github.com>
2024-06-10 11:00:09 -0300

    Add files via upload

---
+++ aulaapi/api/config.php
@@ -0,0 +1,6 @@
+<?php
+        // aqui será a nossa api
+        define('API_IS_ACTIVE', true);
+        define('API_VERSION', '1.0.0');
+        
+?>
---
+++ aulaapi/app/status/index.php
@@ -0,0 +1,17 @@
+<?php
+    // Aqui vai ser nossa aplicação
+    require_once __DIR__ . '../../../api/config.php';
+    require_once __DIR__ . '../../../api/response.php';
+
+    if(API_IS_ACTIVE){
+        echo Response::resposta(200,'success',[
+            'version' => API_VERSION,
+            'status' => 'ATIVA'
+        ]);
+    } else {
+        echo Response::resposta(200,'success',[
+            'version' => API_VERSION,
+            'status' => 'MANUTENÇÃO'
+        ]);
+    }
+ ?>
