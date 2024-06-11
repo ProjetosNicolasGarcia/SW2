5517bbc0f1bd68cb0ecf7c049eed2c0b441d574d
Nicolas Garcia <100208523+ProjetosNicolasGarcia@users.noreply.github.com>
2024-06-11 08:21:02 -0300

    Add files via upload

---
+++ aulaapi/api/data.php
@@ -0,0 +1,55 @@
+<?php
+
+
+return [
+    [
+        'nome' => 'Nicolas',
+        'email' => 'nicolas@gmail.com'
+        
+    ],
+
+    [
+        'nome' => 'Guilherme',
+        'email' => 'Guilherme@email.com'
+    ],
+
+    [
+        'nome' => 'Victor',
+        'email' => 'Olokolegal@hotmail.com'
+    ],
+
+    [
+        'nome' => 'Júlia',
+        'email' => 'julia@gmail.com'
+    ],
+
+    [
+        'nome' => 'Giovanni',
+        'email' => 'giodarosquinha@hotmail.com'
+    ],
+
+    [
+        'nome' => 'Marcus',
+        'email' => 'marcusgoiaba@email.com'
+    ],
+
+    [
+        'nome' => 'Lucas',
+        'email' => 'luquetas@gmail.com'
+    ],
+
+    [
+        'nome' => 'Paiva',
+        'email' => 'paivinha@hotmail.com'
+    ],
+
+    [
+        'nome' => 'Kaike',
+        'email' => 'feio@gmail.com'
+    ],
+
+    [
+        'nome' => 'Fellipe',
+        'email' => 'Manaia@gmail.com'
+    ],
+];
