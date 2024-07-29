64dbb573d734a7772e41f02cea66267099f65015
Nicolas Garcia <100208523+ProjetosNicolasGarcia@users.noreply.github.com>
2024-07-29 11:09:38 -0300

    Add files via upload

---
+++ exercicio/conexao.php
@@ -0,0 +1,17 @@
+<?php
+    $host = 'localhost';
+    $banco = 'etecmcm';
+    $usuario = 'root';
+    $senha = '';
+
+    $conexao = new mysqli($host,$usuario,$senha,$banco);
+
+    if($conexao->connect_error){
+        die('falha de conexão: '. $conexao->connect_error);
+    }
+    
+    // else{
+    //      echo "conectado com sucesso";
+    //}
+
+?>
