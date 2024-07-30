9f095d6ac9166c2f88353ea8d0898cc4975a255a
Nicolas Garcia <100208523+ProjetosNicolasGarcia@users.noreply.github.com>
2024-07-30 08:21:12 -0300

    Add files via upload

--- exercicio/conexao.php
+++ exercicio/conexao.php
@@ -1,17 +1,20 @@
 <?php
-    $host = 'localhost';
-    $banco = 'etecmcm';
+    $host = 'localhost';    
     $usuario = 'root';
     $senha = '';
+    $banco = 'etecmcm';
 
     $conexao = new mysqli($host,$usuario,$senha,$banco);
 
     if($conexao->connect_error){
-        die('falha de conexão: '. $conexao->connect_error);
-    }
+        die('Falha de conexão: ' . $conexao->connect_error);
+      }
+
+      
+     //else{
+    //    echo "CONECTADO COM SUCESSO";
+   // }
     
-    // else{
-    //      echo "conectado com sucesso";
-    //}
+
 
 ?>
