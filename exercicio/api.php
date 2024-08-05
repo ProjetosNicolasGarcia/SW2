df7e69f89c9d67b9c1d2af5bf9526221baa01eae
Nicolas Garcia <100208523+ProjetosNicolasGarcia@users.noreply.github.com>
2024-08-05 10:55:55 -0300

    Add files via upload

--- exercicio/api.php
+++ exercicio/api.php
@@ -3,9 +3,19 @@
     include 'conexao.php';
 
     $metodo = $_SERVER['REQUEST_METHOD'];
-
     echo json_encode($metodo);
 
-    
+    // $url = $_SERVER['REQUEST_URI'];
 
-?>  
+    // //echo json_encode($url);
+
+    // $path = parse_url($url, PHP_URL_PATH);
+    // $path = trim($path,'/');
+
+    // //echo json_encode($path);
+
+    // $pathparts = explode('/',$path);
+
+    // echo json_encode($pathparts[1]);
+
+?>
